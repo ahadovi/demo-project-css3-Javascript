5556b0b66bd892e6f699dac199296ca70a710090
ahadovi <wstoreg@gmail.com>
2021-01-07 15:32:46 +0600

    image upload & input two way binding feature added

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,14 @@ const ovrlaytxt = document.querySelector('.overlay-text');
 const demoText = document.getElementById('demoText');
 const uploadInput = document.querySelector('#uploadInput');
 const animationBtnWrap = document.querySelector('.animation-btn-wrap');
+
+// Upload Images File Input
+uploadInput.addEventListener('change', (e) => {
+    const previewImage = document.querySelector('#thumbnail');
+    previewImage.src = URL.createObjectURL(e.target.files[0]);
+});
+
+// Input two way Bindings
+demoText.addEventListener('keyup', (e) => {
+    ovrlaytxt.innerText = e.target.value;
+});
