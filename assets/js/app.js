1bf93ea3996248b5828561f463534cb425c7d2a0
ahadovi <wstoreg@gmail.com>
2021-01-07 15:35:14 +0600

    overlay text style in different font & it's click button feature added

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,59 @@ uploadInput.addEventListener('change', (e) => {
 demoText.addEventListener('keyup', (e) => {
     ovrlaytxt.innerText = e.target.value;
 });
+
+
+// Text Style & Button Classes Array
+const textBtnClassContent = [
+    {
+        btnClass: 'btnPrimary active',
+        btnContent: 'Montserrat',
+        fontFamily: `'Montserrat', sans-serif  `,
+    },
+    {
+        btnClass: 'btnSecondary',
+        btnContent: 'Open Sans',
+        fontFamily: `'Open Sans', sans-serif`
+    },
+    {
+        btnClass: 'btnSuccess',
+        btnContent: 'Lato',
+        fontFamily: `'Lato', sans-serif`
+    },
+    {
+        btnClass: 'btnDanger',
+        btnContent: 'Raleway',
+        fontFamily: `'Raleway', sans-serif`
+    },
+    {
+        btnClass: 'btnWarning',
+        btnContent: 'Lora',
+        fontFamily: `'Lora', serif`
+    },
+    {
+        btnClass: 'btnInfo',
+        btnContent: 'Roboto',
+        fontFamily: `'Roboto', sans-serif`
+    },
+]
+
+// Text Style Button & It's Content
+textBtnClassContent.forEach( e => {
+    // Creating Button Inside Text button Wrap
+    const textBtn = document.createElement('button');
+    // Add Classes in Text changes Buttons
+    textBtn.classList = `btn ${e.btnClass}`;
+    // Add Style different font family
+    textBtn.style.fontFamily = e.fontFamily;
+
+    // Add Contents in Text changes Buttons
+    textBtn.innerText = e.btnContent;
+    // button DOM Show
+    txtBtnWrap.appendChild(textBtn);
+
+    // Every Button click changes overlay text Font
+    textBtn.addEventListener('click',(el) => {
+        ovrlaytxt.style.fontFamily = e.fontFamily;
+        activeClass(el, 'text-btn-wrap');
+    });
+});
