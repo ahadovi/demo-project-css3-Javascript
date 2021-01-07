57b411f15d7280995c8f86e3ca57b8c5e6e93837
ahadovi <wstoreg@gmail.com>
2021-01-07 15:31:03 +0600

    Common UI Selectors for dom js file

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+// UI Selectors
+const txtBtnWrap = document.querySelector('.text-btn-wrap');
+const ovrlaytxt = document.querySelector('.overlay-text');
+const demoText = document.getElementById('demoText');
+const uploadInput = document.querySelector('#uploadInput');
+const animationBtnWrap = document.querySelector('.animation-btn-wrap');
